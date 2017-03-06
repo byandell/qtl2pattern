a522a8a0bf9d0f8b0fc6212ae575ac63f63042e6
Brian Yandell <brianyandell@Brians-MacBook-Pro.local>
2017-03-06 14:13:48 -0600

    remove snpscan_pattern (used to be called by scan_pattern; deprecated)

---
+++ R/listof_scan1coeff.R
@@ -0,0 +1,103 @@
+#' List of scan1coef objects
+#'
+#' Create a list of scan1coef objects using \code{\link[qtl2scan]{scan1coef}}.
+#'
+#' @param phe data frame of phenotypes
+#' @param probs genotype probabilities object for one chromosome from \code{\link[qtl2geno]{calc_genoprob}}
+#' @param K list of length 1 with kinship matrix
+#' @param covar matrix of covariates
+#'
+#' @return object of class \code{listof_scan1coeff}
+#'
+#' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
+#' @keywords utilities
+#'
+#' @examples
+#' \dontrun{listof_scan1coef(probs, phe)}
+#'
+#' @export
+#' @importFrom qtl2scan scan1coef
+listof_scan1coef <- function(probs, phe, K=NULL, covar=NULL) {
+  eff <- list()
+  phename <- dimnames(phe)[[2]]
+  for(pheno in phename)
+    eff[[pheno]] <- qtl2scan::scan1coef(probs, phe[, pheno, drop=FALSE], K, covar)
+  class(eff) <- c("listof_scan1coef", class(eff))
+  eff
+}
+
+#' Summary of object of class listof_scan1coeff
+#'
+#' Summary of object of class \code{\link{listof_scan1coeff}}, which is a list of objects of class \code{scan1coef}.
+#'
+#' @param object object of class \code{listof_scan1coeff}
+#' @param scan1_object object from \code{scan1}
+#' @param coef_names names of effect coefficients (default is all coefficient names)
+#' @param center center coefficients if \code{TRUE}
+#' @param ... arguments for \code{\link[qtl2scan]{plot_coef}}
+#'
+#' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
+#' @keywords utilities
+#'
+#' @rdname listof_scan1coef
+#' @export
+#' @importFrom dplyr bind_cols
+summary_listof_scan1coef <-
+  function(object, scan1_object,
+           coef_names = dimnames(object[[1]]$coef)[[2]],
+           center = TRUE,
+           ...) {
+  phename <- names(object)
+  chr_id <- names(scan1_object$map)
+  sum_chr <- summary(scan1_object, phename, chr_id)
+  pos <- sum_chr$pos[match(phename, sum_chr$pheno)]
+  names(pos) <- phename
+  wh <- apply(scan1_object$lod,2,function(x) which.max(x)[1])
+  sum_coef <- as.data.frame(matrix(NA,
+                                   nrow(sum_chr),
+                                   length(coef_names),
+                                   dimnames=list(NULL,coef_names)))
+  for(pheno_id in phename) {
+    if(!is.null(object[[pheno_id]])) {
+      ## Need to save these in data frame.
+      tmp <- object[[pheno_id]]$coef[wh[pheno_id],seq_along(coef_names)]
+      if(center)
+        tmp <- tmp - mean(tmp)
+      sum_coef[match(pheno_id, sum_chr$pheno),] <- tmp
+    }
+  }
+  dplyr::bind_cols(sum_chr,sum_coef)
+}
+#' @method summary listof_scan1coef
+#' @rdname listof_scan1coef
+#' @export
+summary.listof_scan1coef <- function(object, ...)
+  summary_listof_scan1coef(object, ...)
+
+#' Summary of object of class listof_scan1coeff
+#'
+#' Summary of object of class \code{\link{listof_scan1coeff}}, which is a list of objects of class \code{scan1coef}.
+#'
+#' @param object object of class \code{listof_scan1coeff}
+#' @param scan1_object object from \code{scan1}
+#' @param ... arguments for \code{\link[qtl2scan]{plot_coef}}
+#'
+#' @author Brian S Yandell, \email{brian.yandell@@wisc.edu}
+#' @keywords utilities
+#'
+#' @rdname listof_scan1coef
+#' @export
+summary_scan1coef <-
+  function(object, scan1_object, ...) {
+    object <- list(object)
+    names(object) <- dimnames(scan1_object$lod)[[2]][1]
+    summary_listof_scan1coef(object, scan1_object, ...)
+  }
+
+#' @method summary scan1coef
+#' @rdname listof_scan1coef
+#' @export
+#' @export summary.scan1coef
+#'
+summary.scan1coef <- function(object, ...)
+  summary_scan1coef(object, ...)
