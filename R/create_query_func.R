9da2ef5a95617c1bb404e97766e2edd99109d8f9
Brian Yandell <byandell@wisc.edu>
2018-01-22 15:57:53 -0600

    migrate create_{probs,mrna}_query_func from DOread

---
+++ R/create_query_func.R
@@ -0,0 +1,14 @@
+#' @export
+create_probs_query_func <- function(dbfile) {
+  function(chr, start, stop, allele = TRUE) {
+    read_probs(chr, start, stop, dbfile, allele, method = "feather")
+  }
+}
+#' @export
+create_mrna_query_func <- function(dbfile) {
+  function(chr, start, stop,
+           local = TRUE,
+           qtl = FALSE) {
+    read_mrna(chr, start, stop, dbfile, local, qtl)
+  }
+}
